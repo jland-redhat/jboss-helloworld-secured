72f11455416f6a3e79d913ff2d07ef489a29f9c7
James Land <hokie10@gmail.com>
2021-01-21 12:27:32 -0500

    Updating

--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "configure-ssl.cli"
-# $JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/configure-ssl.cli
+$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/configure-ssl.cli
